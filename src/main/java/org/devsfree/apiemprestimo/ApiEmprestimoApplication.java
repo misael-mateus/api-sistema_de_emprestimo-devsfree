9fc7f708282f31846c6ff28ef4c57fa56b20aeaa
misael mateus <misaelmateus.java@gmail.com>
2022-07-19 00:45:11 -0400

    Subindo proj para o github

---
+++ src/main/java/org/devsfree/apiemprestimo/ApiEmprestimoApplication.java
@@ -0,0 +1,13 @@
+package org.devsfree.apiemprestimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmprestimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmprestimoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/devsfree/apiemprestimo/ApiEmprestimoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devsfree.apiemprestimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmprestimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
